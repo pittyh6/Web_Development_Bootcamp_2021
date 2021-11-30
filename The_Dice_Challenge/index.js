1a17d7ec9b0cf59c841204c7800028ccec1c258f
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2021-12-01 08:09:19 +1100

    [WDB-204] add randon number to the img dice 2

--- The_Dice_Challenge/index.js
+++ The_Dice_Challenge/index.js
@@ -2,3 +2,7 @@
 var randomNumber1 = Math.floor(Math.random(5) * 6 + 1);
 document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
 console.log(randomNumber1);
+
+var randomNumber2 = Math.floor(Math.random(5) * 6 +1);
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png")
+console.log(randomNumber2);
