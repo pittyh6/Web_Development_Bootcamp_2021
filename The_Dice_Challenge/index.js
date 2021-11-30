032c400dd135d9afb3f7c3fca192e1b1f83f04d9
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2021-12-01 08:37:46 +1100

    [WDB-206] refactoring code

--- The_Dice_Challenge/index.js
+++ The_Dice_Challenge/index.js
@@ -1,16 +1,20 @@
 
 var randomNumber1 = Math.floor(Math.random(5) * 6 + 1);
 document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-console.log(randomNumber1);
 
 var randomNumber2 = Math.floor(Math.random(5) * 6 +1);
 document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png")
-console.log(randomNumber2);
 
-if(randomNumber1 > randomNumber2){
-    document.querySelector("h1").textContent = "Player 1 Wins! 🚩"
-}else if( randomNumber1 < randomNumber2){
-    document.querySelector("h1").textContent = "Player 2 Wins! 🚩"
-}else{
-    document.querySelector("h1").textContent = "Draw"
+function winnerLoser(randomNumber1, randomNumber2){
+    if(randomNumber1 > randomNumber2){
+        document.querySelector("h1").textContent = "Player 1 Wins! 🚩"
+    }else if( randomNumber1 < randomNumber2){
+        document.querySelector("h1").textContent = "Player 2 Wins! 🚩"
+    }else{
+        document.querySelector("h1").textContent = "Draw"
+    }
 }
+winnerLoser(randomNumber1, randomNumber2);
+
+
+
