6465a634cafa3ac69a5a49f1d0cd57983d2ebc0b
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2021-12-02 14:16:43 +1100

    [WDB-246] select same btn by id, animate btn, play sound according to the color select random

--- Simon_Game_project/game.js
+++ Simon_Game_project/game.js
@@ -2,10 +2,26 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
 function nextSequence() {
+    /* random number 0-3 */
     var randomNumber = (Math.floor(Math.random() * 4));
-    
+    console.log(randomNumber + " randomNumber");
+
+    /*Select color using the random number */
     var randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
-    console.log(gamePattern);
+    console.log(randomChosenColour + " randomChosenColour")
     
+    /* add rancom colour to the gamePattern array */
+    gamePattern.push(randomChosenColour);
+    console.log(gamePattern + " gamePatter");
+  
+   /* Get element btn with same ID as in random Choose Colour */
+    var btnChooseRandomByThs = $("#"+randomChosenColour);
+
+     /* Get the same btn as color choose random  */
+    $(btnChooseRandomByThs).fadeOut(100).fadeIn(100);
+
+    /* Play sound according to color */
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
 }
+
