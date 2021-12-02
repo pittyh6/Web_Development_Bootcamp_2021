58bd8f14bc49909a47330f643647a841e25d55ef
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2021-12-02 16:52:04 +1100

    [WDB-252] create class animate -> animate clicked buttons...

--- Simon_Game_project/game.js
+++ Simon_Game_project/game.js
@@ -2,36 +2,41 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
-$(".btn").on("click", function(event){
+$(".btn").on("click", function (event) {
     var userChosenColour = event.target.id;
     userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
-
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
 });
 
 function nextSequence() {
     /* random number 0-3 */
     var randomNumber = (Math.floor(Math.random() * 4));
-    console.log(randomNumber + " randomNumber");
 
     /*Select color using the random number */
     var randomChosenColour = buttonColours[randomNumber];
-    console.log(randomChosenColour + " randomChosenColour")
-    
+
     /* add rancom colour to the gamePattern array */
     gamePattern.push(randomChosenColour);
-    console.log(gamePattern + " gamePatter");
-  
-   /* Get element btn with same ID as in random Choose Colour */
-    var btnChooseRandomByThs = $("#"+randomChosenColour);
 
-     /* Get the same btn as color choose random  */
+    /* Get element btn with same ID as in random Choose Colour */
+    var btnChooseRandomByThs = $("#" + randomChosenColour);
+
+    /* Get the same btn as color choose random  */
     $(btnChooseRandomByThs).fadeOut(100).fadeIn(100);
 
-    /* Play sound according to color */
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
-
-
+    playSound(randomChosenColour)
 }
 
+function playSound(name) {
+    /* Play sound according to color */
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $("#"+ currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#"+ currentColour).removeClass("pressed");
+    }, 100)
+}
