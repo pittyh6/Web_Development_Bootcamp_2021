13b035fab5e0a425954efad6dda22a80441a0781
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2021-12-19 15:11:59 +1100

    [wdb-228]

--- my_express_server/server.js
+++ my_express_server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send("Hi Thereeeeee")
+})
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 })
