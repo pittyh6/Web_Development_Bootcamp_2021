26fff3e47d98e5d3cb8a4dbe4f1b1baa346a8f87
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2021-12-20 06:03:49 +1100

    [wdb-235] post method

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const port = 3000; // default
+const bodyParser = require('body-parser');
+
 app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+const port = 3000; // default
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+    var num1 = Number(req.body.num1)
+    var num2 = Number(req.body.num2)
+    var result = num1 + num2;
+
+    res.send("Result: " + result);
+})
 
 app.listen(port, function(){
     console.log('listening on port 3000');
