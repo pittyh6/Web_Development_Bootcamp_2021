1b8c9f28572fb382695d0e05f79015733d139712
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2021-12-19 15:43:25 +1100

    [wdb-230]

--- my_express_server/server.js
+++ my_express_server/server.js
@@ -5,6 +5,13 @@ app.get('/', (req, res) => {
     res.send("Hi Thereeeeee")
 })
 
+app.get("/contact", (req, res) => {
+    res.send("contact me at pittyh6@gmail.com")
+})
+
+app.get("/about", (req, res) => {
+    res.send("<h1>my name is Priscila</h1> <p>I am a beginner web developer</p> <h3>Give me a job please!!!!</h3>")
+})
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 })
