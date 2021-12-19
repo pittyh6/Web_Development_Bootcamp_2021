f1e38a58ad11f822397da116eb2b5dce0bab35cd
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2021-12-19 12:23:50 +1100

    [wdb-224] npm install superheroes and supervillans

--- intro_to_node/index.js
+++ intro_to_node/index.js
@@ -2,6 +2,14 @@
 
 // import fs from "fs";
 const fs = require("fs");
-
 // copy the content inside file1.txt and create a new file call file2.txt
 fs.copyFileSync("file1.txt", "file2.txt")
+
+const superheroes = require("superheroes")
+const supervillains = require("supervillains")
+
+var super_heroes_name = superheroes.random();
+console.log("my superhero is " + super_heroes_name);
+
+var super_villan_name = supervillains.random();
+console.log("The super villan " + super_villan_name + " is here...");
