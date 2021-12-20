8c745482437b7e83c1fec34ae64b61bd1088d49b
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2021-12-21 06:00:59 +1100

    [wdb-244]

--- weather_project/app.js
+++ weather_project/app.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const https = require('https');
+
 const app = express();
 
 app.get("/", function(req, res) {
     const url= "https://api.openweathermap.org/data/2.5/weather?q=Sydney&units=metric&appid=3d45d101d518ad0739e9b77b6c0f23fd"
     https.get(url, response =>{
-        console.log(response)
+        console.log(response);
+        console.log(response.statusCode);
+
+        response.on('data', function(data){
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
+            
+            //get the weather temperature
+            const temp = weatherData.main.temp;
+            console.log(temp);
+            //get the description
+            const weatherDesc = weatherData.weather[0].description;
+            console.log(weatherDesc);
+
+        })
     })
 
-    res.send("Hello")
-})
+    res.send("Hello");
+});
 
 
 
