7eecd78924a09eea94a4f90ed857282a2b5a7ee3
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2021-12-22 05:54:34 +1100

    [wdb-250]

--- newsletter_signup/app.js
+++ newsletter_signup/app.js
@@ -37,6 +37,12 @@ app.post("/", function (req, res) {
     }
 
     const request = https.request(url, options, function(response){
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html")
+        }else{
+            res.sendFile(__dirname + "/failure.html")
+        }
+
         response.on("data", function(data){
             console.log(JSON.parse(data));
         })
@@ -45,6 +51,10 @@ app.post("/", function (req, res) {
     request.end();
 })
 
+
+app.post("/failure", function(req, res){
+    res.redirect("/")
+})
 
 app.listen(3000, function () {
     console.log('Server is listening on port 3000')
