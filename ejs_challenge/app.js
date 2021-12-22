6198248a0b1285f3353adbf3e5d0c244d3ea507f
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2021-12-23 05:27:10 +1100

    [wdb-278][wdb-280]

--- ejs_challenge/app.js
+++ ejs_challenge/app.js
@@ -16,6 +16,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get("/", function(req, res){
+  res.render('home', {startingContent: homeStartingContent});
+  // res.render(__dirname + "/views/home.ejs");
+})
 
 
 
