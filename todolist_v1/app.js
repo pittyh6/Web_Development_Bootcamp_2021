fbe4bb562a2ba8af286953dfc02d4a5754377abb
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2021-12-22 16:15:29 +1100

    [wdb-270] style to do list external file

--- todolist_v1/app.js
+++ todolist_v1/app.js
@@ -7,6 +7,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
     let today = new Date();
@@ -21,9 +22,9 @@ app.get("/", function(req, res) {
 })
 
 app.post("/", function(req, res) {
-    let item = req.body.newItem
+    let item = req.body.newItem;
     items.push(item);   
-    res.redirect("/")
+    res.redirect("/");
 })
 
 app.listen(3000, function(){
